2ac0937fdb4ee9c371da7828b3e445b175a941b7
bryan-casey-BC <bryan.casey@bellevuecollege.edu>
2025-03-04 22:58:27 +0000

    Removed page 3 with clock

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 from pages.page_1 import func_page_1
 from pages.page_2 import func_page_2
-from pages.page_3 import func_page_3
 
 def main():
     st.sidebar.subheader('Page selection')
     page_selection = st.sidebar.selectbox('Please select a page',['Main Page',
-    'Page 1','Page 2','Page 3'])
+    'Page 1','Page 2'])
     pages_main = {
         'Main Page': main_page,
         'Page 1': run_page_1,
         'Page 2': run_page_2,
-        'Page 3': run_page_3
     }
 
     # Run selected page
@@ -23,8 +21,6 @@ def run_page_1():
     func_page_1()
 def run_page_2():
     func_page_2()
-def run_page_3():
-    func_page_3()
 
 if __name__ == '__main__':
     main()
