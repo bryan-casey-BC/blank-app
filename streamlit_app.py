80b27b54ef5948a21ec601f4bb8af907de038005
bryan-casey-BC <bryan.casey@bellevuecollege.edu>
2025-03-10 03:29:52 +0000

    trying to fix snowflake config

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 
-conn = st.connection("snowflake")
+conn = st.connection("connections.snowflake")
 df = conn.query("SELECT * FROM mytable;", ttl="10m")
 
 for row in df.itertuples():
